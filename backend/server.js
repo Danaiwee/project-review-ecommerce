54c428cde73b33f591f213dc487f421c69b027fe
DanaiWee <Danai_wee@hotmail.com>
2025-07-30 21:12:54 +0700

    Fixed bug 5

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import path from "path";
 
-// import { connectDB } from "./lib/connectDB.js";
+import { connectDB } from "./lib/connectDB.js";
 
-// import authRoutes from "./routes/auth.route.js";
-// import productRoutes from "./routes/product.route.js";
-// import cartRoutes from "./routes/cart.route.js";
-// import couponRoutes from "./routes/coupon.route.js";
-// import paymentRoutes from "./routes/payment.route.js";
-// import analyticsRoutes from "./routes/analytics.route.js";
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
 dotenv.config();
 
@@ -20,25 +20,25 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-// app.use(express.json({ limit: "10mb" }));
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/products", productRoutes);
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/coupon", couponRoutes);
-// app.use("/api/payments", paymentRoutes);
-// app.use("/api/analytics", analyticsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
@@ -50,5 +50,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT 5000`);
-  // connectDB();
+  connectDB();
 });
