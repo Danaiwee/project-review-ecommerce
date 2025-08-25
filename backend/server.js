45fbb60a384f090e33737347038f2299c0811fb9
DanaiWee <Danai_wee@hotmail.com>
2025-08-25 19:56:39 +0700

    Fixed middleware function typepo

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use();
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
